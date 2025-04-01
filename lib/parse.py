650305243f6c4924b97b47425e36566aa686d031
righ1113 <34955220+righ1113@users.noreply.github.com>
2025-04-01 18:08:17 +0900

    25/04/01 整頓2

--- lib/parse.py
+++ lib/parse.py
@@ -1,19 +1,19 @@
-from typing import Any, TypeAlias
+from typing import TypeAlias
 import re
 
 Expr: TypeAlias = int | bool | str | list['Expr']
 
 def run_before(str0: str) -> list[Expr]:
-  def run_before_sub(data: list[str], acc: list[Expr]) -> Any:
+  def run_before_sub(data: list[str], acc: list[Expr]) -> tuple[list[Expr], list[str]]:
     match data:
-      case []: return acc
+      case []: return acc, []
       case [x, *xs]:
         match x:
           case '(':
-            xs2, acc2 = run_before_sub(xs, [])
+            acc2, xs2 = run_before_sub(xs, [])
             return run_before_sub(xs2, acc + [acc2])
           case ')':
-            return xs, acc
+            return acc, xs
           case _:
             match x:
               case ':q':
@@ -29,9 +29,10 @@ def run_before(str0: str) -> list[Expr]:
                   return run_before_sub(xs, acc + [-int(x[1:])])
                 else:
                   return run_before_sub(xs, acc + [x])
+    raise ValueError('not match')
   str1 = re.sub(r';.*', '', str0)                     # コメント削除
   str2 = str1.replace('[]', '(:q)')                   # 空リストを変換
   str3 = str2.replace('[', '(') .replace(']', ' :q)') # 括弧変換
   str4 = str3.replace('(', '( ').replace(')', ' )')   # スペース追加
   str5 = str4.replace('\n', ' ').replace('\r', '')    # 改行削除
-  return run_before_sub(str5.split(), [])
+  return run_before_sub(str5.split(), [])[0]
